ec14df25eda3d5b7a3fe7e4291c25a09f5d41d94
Mayur Tekale <mayurtekale7@gmail.com>
2024-08-08 22:03:07 +0530

    create toggle button and apply js

---
+++ script.js
@@ -0,0 +1,35 @@
+//toggle icon nav
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick =() => {
+    menuIcon.classList.toggle('bx-x');
+}
+
+
+//scroll section
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () =>{
+    section.forEach(sec  => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            //active navbarlinks
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    })
+    let header =document.querySelector('header');
+
+
+    //sticky header
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
